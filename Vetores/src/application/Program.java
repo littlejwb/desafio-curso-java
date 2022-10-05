3f6edca1545d522b781f6930434bd5232af155e0
Juliana Ricardo <szric.ju@gmail.com>
2022-10-05 01:46:49 -0300

    Updating projects

---
+++ Vetores/src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Rent[] vect = new Rent[10];
+		
+		System.out.print("How many rooms will be rented? ");
+		int N = sc.nextInt();
+		
+		for (int i=1; i<=N; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.next();
+			System.out.print("Room: ");
+			int roomNumber = sc.nextInt();
+			
+			Rent rent = new Rent(name, email);
+			
+			vect[roomNumber] = rent; 
+		}
+	
+		System.out.println();
+		System.out.println("Busy rooms: ");
+		
+		for (int i=0; i<vect.length; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
